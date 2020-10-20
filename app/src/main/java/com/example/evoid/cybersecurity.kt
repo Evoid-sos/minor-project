4cc9c935130fbf941b794f89b4e3e8d31d0b7392
eshikaka <agarwaeshi@gmail.com>
2020-10-20 23:59:40 +0530

    shifted guidelines text to strings.xml

--- app/src/main/java/com/example/evoid/cybersecurity.kt
+++ app/src/main/java/com/example/evoid/cybersecurity.kt
@@ -14,14 +14,8 @@ class cybersecurity : AppCompatActivity() {
         reportLink.movementMethod= LinkMovementMethod.getInstance()
         cybercrime.setOnClickListener()
         {
-            showGuidelines.text="1. Make the passwords more complicated by combining letters, numbers, special characters (minimum 10 characters in total) and change them on a regular basis.\n\n" +
-                    "2. Use anti-virus/malware software\n\n" +
-                    "3. Prevent spyware from infiltrating your computer by installing and updating anti-spyware software.\n\n" +
-                    "4. Make sure your social networking profiles (e.g. Facebook, Twitter, YouTube, MSN, etc.) are set to private.\n\n" +
-                    "5. Be careful what information you post online. Once it is on the Internet, it is there forever\n\n" +
-                    "6. Turn on automatic updates to prevent potential attacks on older software.\n\n" +
-                    "7. Public Wi-Fi, a.k.a. “Hot Spots”, are also vulnerable. Avoid conducting financial or corporate transactions on these networks.\n\n" +
-                    "8. Always think before you click on a link or file of unknown origin.\n\n\n\n\n\n\n"
+
+            showGuidelines.text= resources.getString(R.string.cyber_security_guidelines_details)
         }
     }
 }
