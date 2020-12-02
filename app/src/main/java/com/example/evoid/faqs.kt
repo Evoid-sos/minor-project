497eeab29801cf5e9894e9f3d502f6f9c55a04f8
Muskan <muskanchhonker2010@gmail.com>
2020-12-02 15:05:09 +0530

    Edits

--- app/src/main/java/com/example/evoid/faqs.kt
+++ app/src/main/java/com/example/evoid/faqs.kt
@@ -13,7 +13,7 @@ class faqs : AppCompatActivity() {
         setContentView(R.layout.activity_faqs)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.title = Html.fromHtml("<font color='#fffffff'> <small>FREQUENTLY ASKED QUESTIONS </font></small>")
-        val one="<b><i>Q: How Does This App Work?</i></b> <br> A:This App Provied Yous With Features To Contact Emergency Service With Just A Tap. If Panic Button Is Pressed, Your Emergency Contacts Will Receive An Alert With Your Location. Please Note That Your Location Is Being Stored To Our Database While This App Is Active To Provide Accurate Results. You Can Also View Guidelines To Protect Yourself In Case Of Emergency.<br><br>" +
+        val one="<b><i>Q: How Does This App Work?</i></b> <br> A:This App Provides You With Features To Contact Emergency Service With Just A Tap. If Panic Button Is Pressed, Your Emergency Contacts Will Receive An Alert With Your Location. Please Note That Your Location Is Being Stored To Our Database While This App Is Active To Provide Accurate Results. You Can Also View Guidelines To Protect Yourself In Case Of Emergency.<br><br>" +
                 "<b><i>Q: Is This App Free To Use?</i></b> <br> A:Yes, This App Is Free To Use. However, SMS Charges May Apply While Sending SMS To Your Emergency Contacts<br><br>" +
                 "<b><i>Q: Do I Need To Login Every time I Use The App?</i></b> <br> A: No, This App Supports One Time Login. You Stay Logged In Until You Press Logout.<br><br>" +
                 "<b><i>Q: Can I Use This App In IPhone?</i></b> <br> A: Currently This App Only Supports Android Mobile Phones and Tablets.<br><br>"+
