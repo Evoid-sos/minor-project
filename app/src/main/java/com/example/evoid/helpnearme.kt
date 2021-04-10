1817ff68ee6d8f79b41aa691012af1257f3aa1b4
Muskan <muskanchhonker2010@gmail.com>
2021-04-10 19:52:59 +0530

    Help near me option added

--- app/src/main/java/com/example/evoid/helpnearme.kt
+++ app/src/main/java/com/example/evoid/helpnearme.kt
@@ -4,6 +4,9 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.view.View
+import kotlinx.android.synthetic.main.activity_cybersecurity.*
+import kotlinx.android.synthetic.main.activity_helpnearme.*
 
 
 class helpnearme : AppCompatActivity() {
@@ -13,9 +16,22 @@ class helpnearme : AppCompatActivity() {
         setContentView(R.layout.activity_helpnearme)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.title =
-            Html.fromHtml("<font color='#fffffff'> <small>FIND HELP NEAR ME  </font></small>")
+        supportActionBar!!.title = Html.fromHtml("<font color='#fffffff'> <small>FIND HELP NEAR ME  </font></small>")
+        policehelp.setOnClickListener {
+            maplayout.visibility= View.VISIBLE
+        }
+        hospitalhelp.setOnClickListener {
+            maplayout.visibility=View.VISIBLE
+        }
+        firehelp.setOnClickListener {
+            maplayout.visibility=View.VISIBLE
+        }
+        oldagehelp.setOnClickListener {
+            maplayout.visibility=View.VISIBLE
+        }
     }
+
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
