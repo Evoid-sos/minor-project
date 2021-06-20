22342fd028e82866f63026628d07daded46f9dde
eshika <agarwaleshi@gmail.com>
2021-06-20 20:20:13 +0530

    update medicalInformation language

--- app/src/main/java/com/example/evoid/medicalInformation.kt
+++ app/src/main/java/com/example/evoid/medicalInformation.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.ActionBar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_faqs.*
 import kotlinx.android.synthetic.main.activity_medical_information.*
 
 class medicalInformation : AppCompatActivity() {
@@ -20,6 +21,7 @@ class medicalInformation : AppCompatActivity() {
         getActionBarLang(supportActionBar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         cardViewMedicalInformation.setBackgroundResource(R.drawable.cardcorners)
+        buttonSetVisibilityHere()
 
         firestoreClass().loadMedicalInformation(nameMedicalInformation, addressMedicalInformation, bloodTypeMedicalInformation,
             allergiesMedicalInformation, medicationsMedicalInformation, medicalNotesMedicalInformation, organDonorMedicalInformation)
@@ -66,6 +68,53 @@ class medicalInformation : AppCompatActivity() {
     {
         val uid = Firebase.auth.currentUser!!.uid
         return uid
+    }
+
+    fun buttonSetVisibilityHere() {
+        var loggedInUser: User
+        mFireStore.collection(constants.USERS)
+            .document(getCurrentUserId())
+            .get().addOnSuccessListener { document ->
+                loggedInUser = document.toObject(User::class.java)!!
+                if (loggedInUser.lang == "en") {
+
+                    labelNameMedicalInformation.setText(R.string.medicalName)
+                    labelAddressMedicalInformation.setText(R.string.address)
+                    labelBloodTypeMedicalInformation.setText(R.string.bloodType)
+                    labelAllergiesMedicalInformation.setText(R.string.allergies)
+                    labelMedicationsMedicalInformation.setText(R.string.medications)
+                    labelMedicalNotesMedicalInformation.setText(R.string.medicalNotes)
+                    labelOrganDonorMedicalInformation.setText(R.string.organDonor)
+                    addressMedicalInformation.hint = getString(R.string.unknown)
+                    bloodTypeMedicalInformation.hint = getString(R.string.bloodTypeHint)
+                    allergiesMedicalInformation.hint = getString(R.string.allergiesHint)
+                    medicationsMedicalInformation.hint = getString(R.string.medicationsHint)
+                    medicalNotesMedicalInformation.hint = getString(R.string.medicalNotesHint)
+                    organDonorMedicalInformation.hint = getString(R.string.unknown)
+                    saveMedicalInformation.text = getString(R.string.update)
+
+                }
+                if (loggedInUser.lang == "hi") {
+
+                    labelNameMedicalInformation.setText(R.string.medicalNameHindi)
+                    labelAddressMedicalInformation.setText(R.string.addressHindi)
+                    labelBloodTypeMedicalInformation.setText(R.string.bloodTypeHindi)
+                    labelAllergiesMedicalInformation.setText(R.string.allergiesHindi)
+                    labelMedicationsMedicalInformation.setText(R.string.medicationsHindi)
+                    labelMedicalNotesMedicalInformation.setText(R.string.medicalNotesHindi)
+                    labelOrganDonorMedicalInformation.setText(R.string.organDonorHindi)
+                    addressMedicalInformation.hint = getString(R.string.unknownHindi)
+                    bloodTypeMedicalInformation.hint = getString(R.string.bloodTypeHintHindi)
+                    allergiesMedicalInformation.hint = getString(R.string.allergiesHintHindi)
+                    medicationsMedicalInformation.hint = getString(R.string.medicationsHintHindi)
+                    medicalNotesMedicalInformation.hint = getString(R.string.medicalNotesHintHindi)
+                    organDonorMedicalInformation.hint = getString(R.string.unknownHindi)
+                    saveMedicalInformation.text = getString(R.string.updateHindi)
+
+
+                }
+            }
+
     }
 
 }
